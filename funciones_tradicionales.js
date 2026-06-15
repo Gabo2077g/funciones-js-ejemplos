1422eda2259242da607a4f3579d37b5f513cbed8
Gabo2077g <lyxpe854@gmail.com>
2026-06-15 14:12:42 -0600

    Agregar ejemplos de funciones JavaScript

---
+++ funciones_tradicionales.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(88, 32));
+
+function res(a, b) {
+  return a - b;
+}
+console.log(res(450, 175));
+
+function multi(a, b) {
+  return a * b;
+}
+console.log(multi(22, 15));
+
+function divi(a, b) {
+  return a / b;
+}
+console.log(divi(900, 36));
